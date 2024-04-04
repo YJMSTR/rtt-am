88f7c68a54cc5616d83cb5a8debe02b8b44a46e5
yjmstr <pyjmstr@gmail.com>
2024-04-04 10:11:46 +0800

    add support for am ARCH=riscv64-nemu

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -2,8 +2,16 @@
 #include <klib.h>
 #include <rtthread.h>
 
+static rt_ubase_t ctx_from, ctx_to;
+
 static Context* ev_handler(Event e, Context *c) {
   switch (e.event) {
+    case EVENT_YIELD:
+      ctx_from = (rt_ubase_t)(&c);
+      Context *ctxto = *(Context **)ctx_to;
+      return ctxto;
+    case EVENT_IRQ_TIMER:
+      return c;
     default: printf("Unhandled event ID = %d\n", e.event); assert(0);
   }
   return c;
@@ -14,10 +22,17 @@ void __am_cte_init() {
 }
 
 void rt_hw_context_switch_to(rt_ubase_t to) {
+  // Context *c = *(Context **)to;
+  // struct rt_thread *thread = rt_thread_self();
+  ctx_to = to;
+  yield();
   assert(0);
 }
 
 void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
+  *(Context **)from = rt_thread_self()->sp;
+  ctx_to = to;
+  yield();
   assert(0);
 }
 
@@ -25,7 +40,19 @@ void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t t
   assert(0);
 }
 
+void wrapper_func(void *tentry, void *parameter, void *texit) {
+  ((void (*)(void *))tentry)(parameter);
+  ((void (*)())texit)();
+}
+
 rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit) {
-  assert(0);
-  return NULL;
+  rt_uint8_t *addr = (rt_uint8_t *)(stack_addr - ((uintptr_t)stack_addr) % sizeof(Context));
+  rt_uint8_t *ctx_addr = (addr - sizeof(Context));
+  Context *ctx = (Context *)ctx_addr;
+  ctx->mepc = (uintptr_t)wrapper_func;
+  ctx->gpr[10] = (uintptr_t)tentry;
+  ctx->gpr[11] = (uintptr_t)parameter;
+  ctx->gpr[12] = (uintptr_t)texit;
+
+  return ctx_addr;
 }
--- bsp/abstract-machine/src/libc.c
+++ bsp/abstract-machine/src/libc.c
@@ -2,21 +2,69 @@
 #include <rtthread.h>
 
 char *strchr(const char *s, int c) {
-  assert(0);
+  char *res = NULL;
+  for (int i = 0; s[i]; i++) {
+    if (s[i] == c) {
+      res = (char *)(s + i);
+      return res;
+    }
+  }
+  return res;
 }
 
 char *strrchr(const char *s, int c) {
-  assert(0);
+  char *res = NULL;
+  for (int i = 0; s[i]; i++) {
+    if (s[i] == c) {
+      res = (char *)(s + i);
+    }
+  }
+  return res;
+  // assert(0);
 }
 
 char *strstr(const char *haystack, const char *needle) {
   return rt_strstr(haystack, needle);
 }
 
+bool isdigit(char x) {
+  return x >= '0' && x <= '9';
+}
+
 long strtol(const char *restrict nptr, char **restrict endptr, int base) {
-  assert(0);
+  long result = 0;
+  int sign = 1;
+  int i = 0;
+
+  // 跳过前导空白字符
+  while ((unsigned char)nptr[i] == ' ') i++;
+
+  // 检查符号
+  if (nptr[i] == '-' || nptr[i] == '+') {
+    sign = (nptr[i] == '-') ? -1 : 1;
+    i++;
+  }
+
+  // 转换数字直到非数字字符或字符串结束
+  while (isdigit((unsigned char)nptr[i]) && nptr[i] != '\0') {
+    result = result * base + (nptr[i] - '0');
+    i++;
+  }
+
+  // 设置 endptr 指向下一个字符
+  if (endptr != NULL) {
+    *endptr = (char *)(nptr + i);
+  }
+
+  return result * sign;
 }
 
 char *strncat(char *restrict dst, const char *restrict src, size_t sz) {
-  assert(0);
+  int i, j;
+  for (i = 0; dst[i]; i++);
+  for (j = 0; j < sz && src[j] != '\0'; j++) {
+    dst[i+j] = src[j];
+  }
+  dst[i+j] = '\0';
+  return dst;
 }
